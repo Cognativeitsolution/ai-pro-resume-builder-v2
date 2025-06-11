2a17f075faf514b8e8deedebb89be80aba09921f
ayesha-cognitive <ayesha.cognitive@gmail.com>
2025-06-11 16:56:57 +0500

    user dashboard started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { BannerData } from "./data";
-import MainBanner from "@/components/Banner/mainBanner";
+// import { BannerData } from "./data";
+// import MainBanner from "@/components/Banner/mainBanner";
+import Sidebar from "@/components/user/userDashboard/userDashboard";
 
 export default function Page() {
   return (
     <>
-      <MainBanner {...BannerData} />
+      {/* <MainBanner {...BannerData} /> */}
+      <Sidebar/>
     </>
   );
 }
---
+++ src/components/user/dashboard/dashboard.tsx
@@ -0,0 +1,10 @@
+import Sidebar from '@/components/user/userDashboard/userDashboard'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
